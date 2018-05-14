4f7f5127bf2700264626435df1d6a4805f1cfa3d
Victoria Meng <nights1ststar@protonmail.com>
2018-05-14 01:38:33 +0000

    automatically backed up by learn

--- db/migrate/008_add_data_to_shows.rb
+++ db/migrate/008_add_data_to_shows.rb
@@ -1,6 +1,6 @@
 class AddDataToShows < ActiveRecord::Migration[4.2]
   def change
     add_column :shows, :day, :string
-    add_column :shows, :season, :day
+    add_column :shows, :season, :string
   end
 end
