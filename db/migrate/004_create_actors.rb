678eb83f33db1d1bbf1bebc7961c432e6f9be86a
Victoria Meng <nights1ststar@protonmail.com>
2018-05-14 01:04:08 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,4 +1,8 @@
 class CreateActors < ActiveRecord::Migration[4.2]
-  def create 
+  def change
+    create_table :actors do |a|
+      a.string :first_name
+      a.string :last_name
+    end
   end
 end
