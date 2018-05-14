b04606e5f98b789b26f09eefddb9cd8791cae12e
Victoria Meng <nights1ststar@protonmail.com>
2018-05-14 01:31:06 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,6 +3,6 @@ class Character < ActiveRecord::Base
   belongs_to :show
 
   def say_that_thing_you_say
-    "#{first_name} #{last_name} always says: catchphrase"
+    "#{name} always says: catchphrase"
   end
 end
