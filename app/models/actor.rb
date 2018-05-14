9b71c926a1863955c1547d4c0eb0b609a5b1c68c
Victoria Meng <nights1ststar@protonmail.com>
2018-05-14 01:25:42 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,7 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    self.characters.all.map do |character|
+    self.characters.map do |character|
       "#{character.name} - #{character.show}"
     end
   end
